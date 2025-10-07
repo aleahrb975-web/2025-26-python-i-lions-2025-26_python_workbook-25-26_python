d96302f80d6e7e76475d3d786f131b1692a0222b
aleahrb975-web <aleahrb975@gmail.com>
2025-10-07 11:08:20 -0400

    21-25

--- ex34-36-40-55.py
+++ ex34-36-40-55.py
@@ -4,6 +4,16 @@ Write a program that reads an integer from the user.
 Then your program should display a message indicating whether
 the integer is even or odd    
 """
+def even_odd():
+    num = int(input("enter a number: "))
+    if num%2==0:
+        print("the number is even")
+    else:
+        print("the number is odd")
+        
+#even_odd()
+
+
 
 """
 Exercise 36:  Vowel or consonant

2b38a98869b6360e518476572dce307becd96295
aleahrb975-web <aleahrb975@gmail.com>
2025-10-07 10:50:27 -0400

    oct 7

---
+++ ex34-36-40-55.py
@@ -0,0 +1,52 @@
+"""
+Exercise 34:  Even or Odd?
+Write a program that reads an integer from the user.  
+Then your program should display a message indicating whether
+the integer is even or odd    
+"""
+
+"""
+Exercise 36:  Vowel or consonant
+In this exercise you will create a program that reads a letter
+of the alphabet from the user.  If the user enters a, e, i, o, or u
+then your program should display a message indicating that the 
+entered letter is a vowel.  If the user enters y then your program
+should display a message indicating that sometimes y is a vowel, and 
+sometimes y is a consonant.  Otherwise your program should display
+a message indicating that the letter is a consonant.
+"""
+
+"""
+Exercise 40:  Name that triangle
+A triangle can be classified based on the lengths of its sides as
+equilateral, isosceles, or scalene.  All 3 sides of an equilateral
+triangle have the same length.  As isosceles triangle has two sides
+that are the same length, and a third side that is a different length.
+If all of the sides have different lengths then the triangle is scalene.
+
+Write a program that reads the lengths of 3 sides of a triangle from the
+user.  Display a message indicating the type of triangle
+
+******  CHALLENGE:
+Perform the same task as above but with angles and not sides.
+"""
+
+"""
+Exercise 55: Cell Phone Bill
+A particular cell phone plan includes 50 minutes of air time and
+50 text messages for $15.00 a month.  Each additional minute of 
+air time costs $0.25 , while additional text messages cost $0.15
+each.  All cell phone bills include an additional charge of $0.44
+to support 911 call centers, and teh entire bill (including the 
+911 charge) is subject to a 5 percent sales tax.
+
+Write a program that reads the number of minutes and text messages
+used in a month from the user.  Display the base charge, additional
+minutes charge (if any), additional text message charge (if any), 
+the 911 fee, tax and total bill amount.  Only display the additional 
+minute and text charges if the user incurred costs in these 
+categories.  Ensure that all of the charges are displayed using 2
+decimal points
+"""
+
+
